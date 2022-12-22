25246af43bc1b807da241d2873f0cb96f3d81569
TotiFlame <santifer4321@gmail.com>
2022-12-22 19:42:25 +0000

    Task 2

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+window.onload = () => {
+  let amenity_list = [];
+  
+  const checkboxes = document.querySelectorAll('input[type=checkbox]');
+
+  const h_amenities = document.querySelector('.amenities > h4');
+
+  checkboxes.forEach(function(checkbox) {
+    checkbox.addEventListener('change', function() {
+      if (this.checked) {
+        amenity_list.push(this.parentNode.textContent);
+      } else {
+        amenity_list.pop(this.parentNode.textContent);
+      }
+    //   console.log(this.parentNode.textContent);
+      h_amenities.innerHTML = amenity_list;
+    });
+  });
+
+};
